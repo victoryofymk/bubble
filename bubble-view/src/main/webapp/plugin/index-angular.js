c41524766114d50a98dd206fa932a8fe22e997a2
wondershoter <starlord.yan@gmail.com>
2018-09-26 15:06:58 +0800

    DELETE 删除旧 web 模块

--- bubble-view/src/main/webapp/plugin/index-angular.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    // 这里放入执行代码
-    var app = angular.module('myApp', []);
-    app.controller('myCtrl', function ($scope) {
-        $scope.firstName = "John";
-        $scope.lastName = "Doe";
-    });
-});
