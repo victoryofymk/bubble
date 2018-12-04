b4722c571e2465355f0ec28e065f7d6b3974398d
wondershoter <starlord.yan@gmail.com>
2018-12-04 14:10:21 +0800

    ADD 新增日志模块

---
+++ bubble-component/bubble-component-log/src/main/java/com/it/ymk/bubble/component/log/annotation/SystemControllerLog.java
@@ -0,0 +1,16 @@
+package com.it.ymk.bubble.component.log.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 自定义注解 拦截controller
+ *
+ * @author yanmingkun
+ * @date 2018-12-04 13:54
+ */
+@Target({ ElementType.PARAMETER, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SystemControllerLog {
+    String description() default "";
+}
