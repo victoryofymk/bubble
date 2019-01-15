60e479babf7deade1d530a115f29325ad7739eb5
wondershoter <starlord.yan@gmail.com>
2019-01-15 14:06:51 +0800

    ADD 新增几个工具类

--- bubble-core/src/main/java/com/it/ymk/bubble/core/utils/DateUtil.java
+++ bubble-core/src/main/java/com/it/ymk/bubble/core/utils/DateUtil.java
@@ -8,6 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -1182,6 +1183,45 @@ public class DateUtil {
         cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day + 1);
         //System.out.print(sdf.format(cal.getTime()));
         return cal.getTime();
+    }
+
+    /**
+     * 生成ISO-8601 规范的时间格式
+     * @param date
+     * @return
+     */
+    public static String formatISO8601DateString(Date date) {
+        String pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+        return DateFormatUtils.format(date, pattern);
+    }
+
+    /**
+     * 获取反时间戳
+     * @return
+     */
+    public static Long getCurrentReverseTime() {
+        long longTime = System.currentTimeMillis() * 1000000 + CalculateUtil.getNext(999999);
+        return Long.MAX_VALUE - longTime;
+    }
+
+    /**
+     * 获取原时间戳
+     * @param reverseTime
+     * @return
+     */
+    public static Long recoverReverseTime(Long reverseTime) {
+        long longTime = Long.MAX_VALUE - reverseTime;
+        return longTime / 1000000;
+    }
+
+    /**
+     * 生成页面普通展示时间
+     * @param date
+     * @return
+     */
+    public static String formatNormalDateString(Date date) {
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+        return DateFormatUtils.format(date, pattern);
     }
 
     public static void main(String[] args) {
